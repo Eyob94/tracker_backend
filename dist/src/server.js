931b967d32a6abf39f42322140f6a12afa117c58
Eyob94 <eyobsh123@gmail.com>
2022-12-09 00:20:22 +0300

    (fix)

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,5 +1,7 @@
 "use strict";
 exports.__esModule = true;
 var app_1 = require("./app");
-var server = app_1.app.listen(5000, function () { });
+var server = app_1.app.listen(5000, function () {
+    console.log("\n\t\t\uD83D\uDFE2Server started...\n\t");
+});
 //# sourceMappingURL=server.js.map
